29e99afd3bd2f9d1cb29369e465cd10d652c0ce8
Barbara280801 <bcarrillo@unsa.edu.pe>
2022-05-09 17:02:49 -0500

    agrego plantilla de hola mundo al repositorio individual

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class helloworld {
+  public static void main(String[] args){
+    System.out.println("Hello world!");
+    System.out.println("Bàrbara Rubì Carrillo Daza");
+    System.out.println("01101000 01101111 01101100 01100001");
+  }
+}
+
